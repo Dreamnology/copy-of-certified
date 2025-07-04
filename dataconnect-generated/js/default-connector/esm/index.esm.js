d8da8d87e03fe5b196123848717d57c614a37003
chatalltonduplooy <chatalltonduplooy@gmail.com>
2025-07-04 21:45:55 +0000

    commit6

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'dreamnology',
+  location: 'us-south1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,7 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'dreamnology',
+  location: 'us-south1'
+};
+exports.connectorConfig = connectorConfig;
